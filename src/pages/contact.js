ec5d55712298871b869db712f1426a6be16ae410
Abhishek Upadhyay <akrupadhyay.met18@itbhu.ac.in>
2021-01-19 16:39:19 +0530

    Added contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,55 +5,33 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from '../components/Reusable/HeroSection'
 import Infoblock from '../components/Reusable/Infoblock'
-import DualInfoblock from '../components/Reusable/DualInfoblock'
-import Coursecart from '../components/Cart/Coursecart'
+import TeamPhotoSection from '../components/Contact/TeamPhotoSection'
 
 
-const IndexPage = ({data}) => (
+const ContactPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     < HeroSection
       img= {data.img.childImageSharp.fluid}
-      title="Welcome to my Server"
-      subtitle="This is my awesome project"
-      heroclass="hero-background"
+      title="Contact Us"
+      subtitle=""
+      heroclass="about-background"
     />
-    <Infoblock heading="About us" url="/about/"/>
-    <Infoblock heading="Services" url="/services/"/>
-    <Coursecart courses={data.courses} />
-    <DualInfoblock heading="Our Team" address="https://images.pexels.com/photos/5540773/pexels-photo-5540773.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+    <Infoblock heading="How Can We Help?"/>
+    <TeamPhotoSection />
   </Layout>
 )
 
 export const query = graphql`
 {
-  img: file(relativePath: { eq: "heromain.png" }) {
+  img: file(relativePath: { eq: "contact.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
-  courses: allContentfulCourses{
-      edges {
-        node {
-          id
-          title
-          category
-          price
-          description {
-            description
-          }
-          image {
-            fixed(width:200, height:120){
-              ...GatsbyContentfulFixed_tracedSVG
-            }
-          }
-        }
-      }
-    }
-    
 }
 `
 
-export default IndexPage
+export default ContactPage
